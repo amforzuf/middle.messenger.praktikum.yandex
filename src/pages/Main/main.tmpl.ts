3b68570e73d1571406e5e83c4e1546dedd300ec1
Amforzuf <black_fox_0@mail.ru>
2023-09-24 09:20:05 +0300

    ul, li added in main.tmpl.ts

--- src/pages/Main/main.tmpl.ts
+++ src/pages/Main/main.tmpl.ts
@@ -1,11 +1,13 @@
 export const tmpl = `
   <nav class='mainLinks'>
-    <a class='mainLink' href="login">Вход</a>
-    <a class="mainLink" href="registration">Регистрация</a>
-    <a class="mainLink" href="profile">Профиль</a>
-    <a class="mainLink" href="change_password">Сменить пароль</a>
-    <a class="mainLink" href="chat">Чат</a>
-    <a class="mainLink" href="notFoundError">404</a>
-    <a class="mainLink" href="serverError">500</a>
+    <ul>
+      <li><a class='mainLink' href="login">Вход</a></li>
+      <li><a class="mainLink" href="registration">Регистрация</a></li>
+      <li><a class="mainLink" href="profile">Профиль</a></li>
+      <li><a class="mainLink" href="change_password">Сменить пароль</a></li>
+      <li><a class="mainLink" href="chat">Чат</a></li>
+      <li><a class="mainLink" href="notFoundError">404</a></li>
+      <li><a class="mainLink" href="serverError">500</a></li>
+    </ul>
   </nav>
 `;
